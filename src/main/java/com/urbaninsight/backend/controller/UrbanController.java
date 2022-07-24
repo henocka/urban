49c159c8778b7fdfb50f5a939fdde53c8e7bf63c
Henock <henock.berhane@vizientinc.com>
2022-07-24 00:28:36 -0500

    new one

---
+++ src/main/java/com/urbaninsight/backend/controller/UrbanController.java
@@ -0,0 +1,14 @@
+package com.urbaninsight.backend.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UrbanController {
+
+    @GetMapping
+    public ResponseEntity<?> healthCheck() {
+        return ResponseEntity.ok("works successfully!");
+    }
+}
